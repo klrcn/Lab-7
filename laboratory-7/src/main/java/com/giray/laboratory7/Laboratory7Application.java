c4131e755cc9fee06e4c88d2adafc237ecbf228d
klrcn <girayfrhancien@gmail.com>
2025-11-19 21:33:16 +0800

    Initial commit

---
+++ laboratory-7/src/main/java/com/giray/laboratory7/Laboratory7Application.java
@@ -0,0 +1,13 @@
+package com.giray.laboratory7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratory7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratory7Application.class, args);
+	}
+
+}
---
+++ laboratory-7/src/test/java/com/giray/laboratory7/Laboratory7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giray.laboratory7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratory7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
